97200db64abd63ed32f9d260761384ee5e2d0d08
Gabriel <thgtutoriais@hotmail.com>
2021-01-04 23:06:19 -0300

    16

--- src/main/java/com/thgplugins/site/SiteApplication.java
+++ src/main/java/com/thgplugins/site/SiteApplication.java
@@ -1,13 +1,22 @@
 package com.thgplugins.site;
 
+import com.thgplugins.site.domain.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class SiteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
+
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("site-jpa");
+//        EntityManager em = emf.createEntityManager();
+
     }
 
 }
---
+++ src/main/java/com/thgplugins/site/domain/Category.java
@@ -0,0 +1,30 @@
+package com.thgplugins.site.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+@Entity
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Category(){}
+
+
+
+}
